8d7b5d042304bd82b26668ad2bdcd4d944c9fc9f
Altiansyah Fanny <altiansyahf.nutech@gmail.com>
2023-11-08 21:04:58 +0700

    first

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,35 @@
+import prismadb from '@/lib/prismadb';
+import { auth } from '@clerk/nextjs';
+import React from 'react';
+import { redirect } from 'next/navigation';
+import Navbar from '@/components/navbar';
+
+const DashboardLayput = async ({
+	children,
+	params,
+}: {
+	children: React.ReactNode;
+	params: { storeId: string };
+}) => {
+	const { userId } = auth();
+
+	if (!userId) redirect('/sign-in');
+
+	const store = await prismadb.store.findFirst({
+		where: {
+			id: params.storeId,
+			userId,
+		},
+	});
+
+	if (!store) redirect('/');
+
+	return (
+		<>
+			<Navbar />
+			{children}
+		</>
+	);
+};
+
+export default DashboardLayput;
